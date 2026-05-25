8d2be1d0d453f68ad7773aece992f5bb25652715
isabellerosedurant3-ux <isabellerosedurant3@gmail.com>
2026-05-25 11:09:44 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,22 +6,5 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: { unoptimized: true },
-  output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization, X-Client-Info, Apikey' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-        ],
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
